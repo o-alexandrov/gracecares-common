b04490c70b32d98e3741c5125029ed127da10def
Olzhas Alexandrov <9992724+o-alexandrov@users.noreply.github.com>
2025-06-20 08:02:12 -0500

    CHANGED: temporarily hardcode endpoint (domain is unavailable)

--- env/src/endpoints.ts
+++ env/src/endpoints.ts
@@ -4,7 +4,8 @@ export const urlScheme = `${process.env.DOMAIN}://` as const
 /**
  * CDN endpoint of the website
  */
-export const endpointWebsite = `${protocol}${process.env.DOMAIN}` as const
+// export const endpointWebsite = `${protocol}${process.env.DOMAIN}` as const
+export const endpointWebsite = `https://d26yatbmmh5vqo.cloudfront.net`
 export const localApiPort = 4444
 export const endpointAPI =
   process.env.NODE_ENV === `development` && !process.env.NATIVE_APP
