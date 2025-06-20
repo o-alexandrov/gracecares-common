eda69f86b42471ac879db0764c2f6d521caf5ffc
Olzhas Alexandrov <9992724+o-alexandrov@users.noreply.github.com>
2025-06-20 09:04:50 -0500

    CHANGED: public assets path

--- helpers/src/common/get-public-asset-pathname.ts
+++ helpers/src/common/get-public-asset-pathname.ts
@@ -1,12 +1,7 @@
-import * as env from "@gracecares-ai/env"
-
 /**
- * When our web app is loaded in a browser, we have a base pathname for assets
- * This function automatically selects the correct pathname based on the environment
+ * In case we add SSR in the future, we would add a prefix for the public assets.
  */
 export const getPublicAssetPathname = (assetPath: string) => {
   const absolutePath = `/${assetPath}`
-  return process.env.NATIVE_APP || process.env.NODE_ENV === `development`
-    ? absolutePath
-    : `/${env.assetsDirectoryName}${absolutePath}`
+  return absolutePath
 }
