9ae2ae1ab86eccce2618e69f487d8fdf10692fd9
Olzhas Alexandrov <9992724+o-alexandrov@users.noreply.github.com>
2025-06-24 09:54:38 -0500

    ADDED: `temp` bucket name

--- env/src/s3.ts
+++ env/src/s3.ts
@@ -7,10 +7,14 @@ export const mimeTypes = [
   `image/avif`,
 ]
 
+export const s3PrefixTemp = `temp`
+export const s3BucketTemp =
+  `${s3PrefixTemp}${process.env.AWS_ACCOUNT_ID}` as const
+
 export const s3PrefixMediaRaw = `media-raw`
 export const s3BucketMediaRaw =
   `${s3PrefixMediaRaw}${process.env.AWS_ACCOUNT_ID}` as const
 /**
  * AWS S3 buckets we have
  */
-export type S3BucketName = typeof s3BucketMediaRaw
+export type S3BucketName = typeof s3BucketMediaRaw | typeof s3BucketTemp
