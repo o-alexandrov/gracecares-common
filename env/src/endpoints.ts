22dbd6664986b4a5b29739309b9914ee19d60d00
Olzhas Alexandrov <9992724+o-alexandrov@users.noreply.github.com>
2025-09-09 01:10:42 -0500

    FIXED: website endpoint

--- env/src/endpoints.ts
+++ env/src/endpoints.ts
@@ -5,7 +5,7 @@ export const urlScheme = `${process.env.DOMAIN}://` as const
  * CDN endpoint of the website
  */
 // export const endpointWebsite = `${protocol}${process.env.DOMAIN}` as const
-export const endpointWebsite = `https://d26yatbmmh5vqo.cloudfront.net`
+export const endpointWebsite = `https://d2kkt4kci8sq0w.cloudfront.net`
 export const localApiPort = 4444
 export const endpointAPI =
   process.env.NODE_ENV === `development` && !process.env.NATIVE_APP
