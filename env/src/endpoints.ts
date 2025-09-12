547458d06f784a1ffbcdd89eb2c682f583ea3423
dev-natalya <37357918+dev-natalya@users.noreply.github.com>
2025-09-12 10:45:22 +0700

    CHANGED: update endpointWebsite for development environment

--- env/src/endpoints.ts
+++ env/src/endpoints.ts
@@ -5,8 +5,14 @@ export const urlScheme = `${process.env.DOMAIN}://` as const
  * CDN endpoint of the website
  */
 // export const endpointWebsite = `${protocol}${process.env.DOMAIN}` as const
-export const endpointWebsite = `https://d2kkt4kci8sq0w.cloudfront.net`
+export const endpointWebsite =
+  process.env.NODE_ENV === `development` && !process.env.NATIVE_APP
+    ? // ? (`${protocol}${process.env.LOCALHOST}:8888` as const)
+      (`${protocol}localhost:8888` as const)
+    : `https://d2kkt4kci8sq0w.cloudfront.net`
+
 export const localApiPort = 4444
+
 export const endpointAPI =
   process.env.NODE_ENV === `development` && !process.env.NATIVE_APP
     ? (`${protocol}${process.env.LOCALHOST}:${localApiPort}/` as const)
